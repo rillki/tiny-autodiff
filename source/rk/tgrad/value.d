7b9daa53bd40b874f22875c8047b21904e13cbc4
Ki Rill <rill.ki@yahoo.com>
2023-12-07 15:11:02 +0600

    update

--- source/rk/tgrad/value.d
+++ source/rk/tgrad/value.d
@@ -1,12 +1,21 @@
 module rk.tgrad.value;
 
 import std.traits : isFloatingPoint;
+import std.container.rbtree : RedBlackTree;
 
 struct Value(T = float) if (isFloatingPoint!T)
 {
     T data;
     T grad;
-    Value!T[2] children;
+    typeof(this)[] children;
+    void delegate(ref typeof(this) grads) backward;
+
+    alias data this;
+
+    this(in T data) {
+        this.data = data;
+        this.grad = 0;
+    }
 }
 
 
