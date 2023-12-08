241356cf1130f1834035b4c3dac74e2f38c42f28
Ki Rill <rill.ki@yahoo.com>
2023-12-08 11:53:28 +0600

    added gradient calculation

--- source/app.d
+++ source/app.d
@@ -5,22 +5,25 @@ import rk.tgrad;
 
 void main()
 {
-    auto a = value(3);
-    auto b = value(-2);
-    auto c = value(12);
-    auto d = a * b;
-    auto e = d + c;
+    auto a = new Value!float(3);
+    auto b = new Value!float(-2);
+    auto d = new Value!float(12);
+    auto c = a * b;
+    auto e = c + d;
+    auto f = e * e;
+    auto g = f / 2;
 
-    e.backward();
-    // d.backward();
-    // c.backward();
-    // b.backward();
-    // a.backward();
+    g.backward();
+    g.resetGrads();
+    g.backward();
+
     writefln("Value(label='%s', data=%s, grad=%s, children=%s)", "a", a.data, a.grad, a.children);
     writefln("Value(label='%s', data=%s, grad=%s, children=%s)", "b",  b.data, b.grad, b.children);
     writefln("Value(label='%s', data=%s, grad=%s, children=%s)", "c",  c.data, c.grad, c.children);
     writefln("Value(label='%s', data=%s, grad=%s, children=%s)", "d",  d.data, d.grad, d.children);
     writefln("Value(label='%s', data=%s, grad=%s, children=%s)", "e",  e.data, e.grad, e.children);
+    writefln("Value(label='%s', data=%s, grad=%s, children=%s)", "f",  f.data, f.grad, f.children);
+    writefln("Value(label='%s', data=%s, grad=%s, children=%s)", "g",  g.data, g.grad, g.children);
 }
 
 
--- source/rk/tgrad/value.d
+++ source/rk/tgrad/value.d
@@ -1,17 +1,13 @@
 module rk.tgrad.value;
 
 import std.traits : isFloatingPoint;
-import std.container.rbtree : RedBlackTree;
 
-alias value = Value!float;
-
-struct Value(T = float) if (isFloatingPoint!T)
+class Value(T = float) if (isFloatingPoint!T)
 {
     T data;
     T grad;
     typeof(this)[] children;
     void delegate() _backward;
-    size_t backwardCount;
 
     alias data this;
 
@@ -19,7 +15,7 @@ struct Value(T = float) if (isFloatingPoint!T)
     {
         this.data = data;
         this.grad = 0;
-        this.backwardCount = 0;
+        this._backward = (){};
     }
 
     this(in T data, typeof(this)[] children) 
@@ -28,62 +24,85 @@ struct Value(T = float) if (isFloatingPoint!T)
         this.children = children;
     }
 
-    typeof(this) opBinary(string op)(typeof(this) rhs) 
+    void backward() {
+        import std.algorithm : canFind;
+
+        // build topo list
+        typeof(this)[] topoList;
+        buildTopoList(this, topoList);
+
+        // perform backward propagation
+        this.grad = 1;
+        foreach (child; topoList)
+        {   
+            child._backward();
+        }
+    }
+
+    void resetGrads() {
+        // build topo list
+        typeof(this)[] topoList;
+        buildTopoList(this, topoList);
+
+        // reset grads
+        foreach (child; topoList)
+        {
+            child.grad = 0;
+        }
+    }
+
+    /// for Value 
+    typeof(this) opBinary(string op)(ref typeof(this) rhs) 
     {
-        auto result = Value!T(mixin("this.data" ~ op ~ "rhs.data"), [this, rhs]);
-        result.grad = 1;
-        result._backward = () {
-            static if (op == "+" || op == "-") {
+        auto result = new Value!T(mixin("this.data" ~ op ~ "rhs.data"), [this, rhs]);
+        result._backward = () 
+        {
+            static if (op == "+" || op == "-") 
+            {
                 this.grad += result.grad;
                 rhs.grad += result.grad;
-            } else static if (op == "*" || op == "/") {
+            } 
+            else static if (op == "*" || op == "/") 
+            {
                 this.grad += mixin("rhs.data" ~ op ~ "result.grad");
                 rhs.grad += mixin("this.data" ~ op ~ "result.grad");
-            } else static assert(0, "Operator <"~op~"> not supported!");
+            } 
+            else static assert(0, "Operator <"~op~"> not supported!");
         };
         return result;
     }
 
-    void backward() {
-        import std.array : array;
-        import std.algorithm : sort, uniq, canFind;
-
-        // build topo list
-        typeof(this)[] topoList = [];
-        void buildTopoList(typeof(this) visitor) 
+    /// for numerical values
+    typeof(this) opBinary(string op)(in T rhs)
+    {
+        auto rhs_value = new Value!T(rhs);
+        auto result = new Value!T(mixin("this.data" ~ op ~ "rhs_value.data"), [this, rhs_value]);
+        result._backward = () 
         {
-            if (!topoList.canFind(visitor)) topoList ~= visitor;
-            foreach (v; visitor.children)
-            {   
-                buildTopoList(v);
-            }
-        }
-        buildTopoList(this);
-
-        // perform backward propagation
-        // grad = 1;
-        foreach (child; topoList)
-        {   
-            if (child._backward) child._backward();
-        }
-
-        // grad = 1;
-        // foreach (typeof(this) child; topoList)
-        // {   
-            
-        // }        
-        // grad = 1;
-        // if (_backward) _backward();
-        // foreach (typeof(this) child; children)
-        // {   
-        //     if (child.backwardCount == 0) child.backward();
-        // }
-        // backwardCount++;
+            static if (op == "+" || op == "-") 
+            {
+                this.grad += result.grad;
+                rhs_value.grad += result.grad;
+            } 
+            else static if (op == "*" || op == "/") 
+            {
+                this.grad += mixin("rhs_value.data" ~ op ~ "result.grad");
+                rhs_value.grad += mixin("this.data" ~ op ~ "result.grad");
+            } 
+            else static assert(0, "Operator <"~op~"> not supported!");
+        };
+        return result;
     }
 
-    void resetGrads() {
-        grad = 0;
-        backwardCount = 0;
+    private void buildTopoList(typeof(this) visitor, ref typeof(this)[] topoList) 
+    {
+        import std.algorithm : canFind;
+
+        if (!topoList.canFind(visitor)) topoList ~= visitor;
+        foreach (v; visitor.children)
+        {   
+            buildTopoList(v, topoList);
+        }
     }
 }
 
