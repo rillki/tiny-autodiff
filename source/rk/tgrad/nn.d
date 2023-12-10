5261ee237e4cfcbf3462a87dc8c588d110d9fb02
Ki Rill <rill.ki@yahoo.com>
2023-12-10 19:34:34 +0600

    update

--- source/rk/tgrad/nn.d
+++ source/rk/tgrad/nn.d
@@ -4,37 +4,6 @@ import rk.tgrad.value;
 import rk.tgrad.activation;
 import std.traits : isFloatingPoint;
 
-template neuron(size_t inSize, T = float)
-{
-    auto neuron(Value!(T) function(Value!T) activate = (x) { return activateRelu(x); })
-    {
-        import std.random : uniform;
-
-        // randomize weights
-        Value!(T)[inSize] weights;
-        foreach (w; weights)
-        {
-            w = value(uniform(0.0f, 1.0f));
-        }
-
-        return new Neuron!(T, inSize)(value(1), weights, activate);
-    }
-}
-
-// auto neuron(T = float)(size_t inSize, void delegate(Value!T) activate = activateRelu)
-// {
-//     import std.random : uniform;
-
-//     // randomize weights
-//     Value!(T)[inSize] weights;
-//     foreach (w; weights)
-//     {
-//         w = value(uniform(0.0f, 1.0f));
-//     }
-
-//     return new Neuron!(T)(value(1), weights, activate);
-// }
-
 interface INeuron
 {
     void zeroGrad();
@@ -85,7 +54,6 @@ class Neuron(T = float, size_t inSize) if (isFloatingPoint!T) : INeuron
 
 unittest
 {
-    // auto n = neuron!(3)();
     auto n = new Neuron!(float, 1)(value(0.0), [value(2)], (x){ return activateRelu(x); });
     auto r = n([value(2)]);
     import std.stdio; r.writeln; writeln(n.parameters());
