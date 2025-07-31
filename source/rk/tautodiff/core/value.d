e689cf00c7c656b65ba606550f7b322617bfd531
Kirill Saidov <kirillsaidovd@yahoo.com>
2025-07-31 10:53:22 +0500

    sub and div backward operations added, previous solutions is only for add/mul and thus is incorrect

--- source/rk/tautodiff/core/value.d
+++ source/rk/tautodiff/core/value.d
@@ -123,28 +123,26 @@ class Value : INeuron
 
     auto opBinary(string op)(Value rhs) 
     {
-        static if (op == "+" || op == "-")
-        {
-            return value(mixin("this.data" ~ op ~ "rhs.data"), op[0], [this, rhs], &opBackwardAddSub);
-        }
-        else static if (op == "*" || op == "/")
-        {
-            return value(mixin("this.data" ~ op ~ "rhs.data"), op[0], [this, rhs], &opBackwardMulDiv);
-        }
+        void function(Value) backward = null;
+        static if (op == "+") backward = &opBackwardAdd;
+        else static if (op == "-") backward = &opBackwardSub;
+        else static if (op == "*") backward = &opBackwardMul;
+        else static if (op == "/") backward = &opBackwardDiv;
         else static assert(0, "Operator <"~op~"> not supported!");
+        
+        return value(mixin("this.data" ~ op ~ "rhs.data"), op[0], [this, rhs], backward);
     }
 
     auto opBinary(string op)(in ElementType rhs)
     {
-        static if (op == "+" || op == "-")
-        {
-            return value(mixin("this.data" ~ op ~ "rhs"), op[0], [this, value(rhs)], &opBackwardAddSub);
-        }
-        else static if (op == "*" || op == "/")
-        {
-            return value(mixin("this.data" ~ op ~ "rhs"), op[0], [this, value(rhs)], &opBackwardMulDiv);
-        }
+        void function(Value) backward = null;
+        static if (op == "+") backward = &opBackwardAdd;
+        else static if (op == "-") backward = &opBackwardSub;
+        else static if (op == "*") backward = &opBackwardMul;
+        else static if (op == "/") backward = &opBackwardDiv;
         else static assert(0, "Operator <"~op~"> not supported!");
+        
+        return value(mixin("this.data" ~ op ~ "rhs"), op[0], [this, value(rhs)], backward);
     }
 
     override string toString() const @safe pure
@@ -155,29 +153,27 @@ class Value : INeuron
 
     auto opBinaryRight(string op)(in ElementType lhs)
     {
-        static if (op == "+" || op == "-")
-        {
-            return value(mixin("lhs" ~ op ~ "this.data"), op[0], [value(lhs), this], &opBackwardAddSub);
-        }
-        else static if (op == "*" || op == "/")
-        {
-            return value(mixin("lhs" ~ op ~ "this.data"), op[0], [value(lhs), this], &opBackwardMulDiv);
-        }
+        void function(Value) backward = null;
+        static if (op == "+") backward = &opBackwardAdd;
+        else static if (op == "-") backward = &opBackwardSub;
+        else static if (op == "*") backward = &opBackwardMul;
+        else static if (op == "/") backward = &opBackwardDiv;
         else static assert(0, "Operator <"~op~"> not supported!");
+        
+        return value(mixin("lhs" ~ op ~ "this.data"), op[0], [value(lhs), this], backward);
     }
 
     /// Inplace operations
     void opInplace(string op)(Value lhs, Value rhs)
     {
-        static if (op == "+" || op == "-")
-        {
-            this.reinit(mixin("lhs.data" ~ op ~ "rhs.data"), op[0], [lhs, rhs], &opBackwardAddSub);
-        }
-        else static if (op == "*" || op == "/")
-        {
-            this.reinit(mixin("lhs.data" ~ op ~ "rhs.data"), op[0], [lhs, rhs], &opBackwardMulDiv);
-        }
+        void function(Value) backward = null;
+        static if (op == "+") backward = &opBackwardAdd;
+        else static if (op == "-") backward = &opBackwardSub;
+        else static if (op == "*") backward = &opBackwardMul;
+        else static if (op == "/") backward = &opBackwardDiv;
         else static assert(0, "Operator <"~op~"> not supported!");
+        
+        this.reinit(mixin("lhs.data" ~ op ~ "rhs.data"), op[0], [lhs, rhs], backward);
     }
     
     /// Re-initialize object parameters
@@ -215,99 +211,172 @@ class Value : INeuron
     }
 }
 
-/// Backward operation for addition and substraction
-void opBackwardAddSub(Value opResult)
+/// Backward operation for addition only
+void opBackwardAdd(Value opResult)
 {
-    // get parents
     auto lhs = opResult.parents[0];
     auto rhs = opResult.parents[1];
 
-    // perform backward operation
     lhs.grad += 1.0 * opResult.grad;
     rhs.grad += 1.0 * opResult.grad;
 }
 
-/// Backward operation for multiplication and division
-void opBackwardMulDiv(Value opResult)
+/// Backward operation for subtraction only
+void opBackwardSub(Value opResult)
 {
-    // get parents
     auto lhs = opResult.parents[0];
     auto rhs = opResult.parents[1];
 
-    // perform backward operation
+    lhs.grad += 1.0 * opResult.grad;
+    rhs.grad += -1.0 * opResult.grad;
+}
+
+/// Backward operation for multiplication only
+void opBackwardMul(Value opResult)
+{
+    auto lhs = opResult.parents[0];
+    auto rhs = opResult.parents[1];
+
     lhs.grad += rhs.data * opResult.grad;
     rhs.grad += lhs.data * opResult.grad;
 }
 
+/// Backward operation for division only
+void opBackwardDiv(Value opResult)
+{
+    auto lhs = opResult.parents[0];
+    auto rhs = opResult.parents[1];
+
+    lhs.grad += (1.0 / rhs.data) * opResult.grad;
+    rhs.grad += (-lhs.data / (rhs.data * rhs.data)) * opResult.grad;
+}
+
 unittest
 {
-    // create
-    auto a = value(2);
-    auto b = value(-3);
-    auto c = value(10);
-    auto f = value(-2);
-    auto e = a * b;
-    auto d = e + c;
-    auto g = f * d;
+    // check sum and mul operations
+    {
+        // create
+        auto a = value(2);
+        auto b = value(-3);
+        auto c = value(10);
+        auto f = value(-2);
+        auto e = a * b;
+        auto d = e + c;
+        auto g = f * d;
 
-    // check values
-    assert(g.data == -8);
-    assert(f.data == -2);
-    assert(d.data == 4);
-    assert(e.data == -6);
-    assert(c.data == 10);
-    assert(b.data == -3);
-    assert(a.data == 2);
+        // check values
+        assert(g.data == -8);
+        assert(f.data == -2);
+        assert(d.data == 4);
+        assert(e.data == -6);
+        assert(c.data == 10);
+        assert(b.data == -3);
+        assert(a.data == 2);
 
-    // check grad
-    assert(g.grad == 0);
-    assert(f.grad == 0);
-    assert(d.grad == 0);
-    assert(e.grad == 0);
-    assert(c.grad == 0);
-    assert(b.grad == 0);
-    assert(a.grad == 0);
+        // check grad
+        assert(g.grad == 0);
+        assert(f.grad == 0);
+        assert(d.grad == 0);
+        assert(e.grad == 0);
+        assert(c.grad == 0);
+        assert(b.grad == 0);
+        assert(a.grad == 0);
 
-    // backward
-    g.backward();
+        // backward
+        g.backward();
 
-    // check grad after backward
-    assert(g.grad == 1);
-    assert(f.grad == 4);
-    assert(d.grad == -2);
-    assert(e.grad == -2);
-    assert(c.grad == -2);
-    assert(b.grad == -4);
-    assert(a.grad == 6);
+        // check grad after backward
+        assert(g.grad == 1);
+        assert(f.grad == 4);
+        assert(d.grad == -2);
+        assert(e.grad == -2);
+        assert(c.grad == -2);
+        assert(b.grad == -4);
+        assert(a.grad == 6);
 
-    // zero grad
-    g.zeroGrad();
-    f.zeroGrad();
-    d.zeroGrad();
-    e.zeroGrad();
-    c.zeroGrad();
-    b.zeroGrad();
-    a.zeroGrad();
-    assert(g.grad == 0);
-    assert(f.grad == 0);
-    assert(d.grad == 0);
-    assert(e.grad == 0);
-    assert(c.grad == 0);
-    assert(b.grad == 0);
-    assert(a.grad == 0);
+        // zero grad
+        g.zeroGrad();
+        f.zeroGrad();
+        d.zeroGrad();
+        e.zeroGrad();
+        c.zeroGrad();
+        b.zeroGrad();
+        a.zeroGrad();
+        assert(g.grad == 0);
+        assert(f.grad == 0);
+        assert(d.grad == 0);
+        assert(e.grad == 0);
+        assert(c.grad == 0);
+        assert(b.grad == 0);
+        assert(a.grad == 0);
 
-    // test parameters property returns by reference
-    g.parameters[0].grad = 2;
-    assert(g.grad == 2);
+        // test parameters property returns by reference
+        g.parameters[0].grad = 2;
+        assert(g.grad == 2);
 
-    // check inplace operation
-    auto h = value(0);
-    h.opInplace!"*"(a, b);
-    a.zeroGrad();
-    b.zeroGrad();
-    h.backward();
-    assert(h.data == -6);
-    assert(b.data == -3);
-    assert(a.data == 2);
+        // check inplace operation
+        auto h = value(0);
+        h.opInplace!"*"(a, b);
+        a.zeroGrad();
+        b.zeroGrad();
+        h.backward();
+        assert(h.data == -6);
+        assert(b.data == -3);
+        assert(a.data == 2);
+    }
+
+    // test sub and div operations
+    {
+        auto x = value(5.0);
+        auto y = value(3.0);
+        auto z = x - y;
+        z.backward();
+        
+        assert(z.data == 2.0);
+        assert(x.grad == 1.0);
+        assert(y.grad == -1.0);
+        
+        // Test subtraction with scalar (right side)
+        x.grad = 0;
+        auto w = x - 2.0;
+        w.backward();
+        
+        assert(w.data == 3.0);
+        assert(x.grad == 1.0);
+        
+        // test subtraction with scalar (left side)  
+        x.grad = 0;
+        auto v = 10.0 - x;
+        v.backward();
+        
+        assert(v.data == 5.0);
+        assert(x.grad == -1.0);
+        
+        // test with a more complex expression
+        auto a = value(4.0);
+        auto b = value(2.0);
+        auto c = value(3.0);
+        
+        // f = (a - b) * c = (4 - 2) * 3 = 6
+        auto f = (a - b) * c;
+        f.backward(); 
+        
+        assert(f.data == 6.0);
+        assert(a.grad == 3.0);
+        assert(b.grad == -3.0);
+        assert(c.grad == 2.0);
+
+        // test division
+        auto g = value(6.0);
+        auto h = value(2.0);
+        auto j = g / h;
+        
+        j.backward();
+        
+        import std.math : abs;
+        assert(j.data == 3.0);
+        assert(abs(g.grad - 0.5) < 1e-10);
+        assert(abs(h.grad - (-1.5)) < 1e-10);
+    }
 }
 
